73901e333d282afb93c5a71d161f1d26c146164c
manirabona-programer <hseal419@gmail.com>
2023-07-18 09:44:04 +0200

    setup: laravel clockwork for debugging

--- config/app.php
+++ config/app.php
@@ -185,6 +185,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Clockwork\Support\Laravel\ClockworkServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -212,7 +213,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        // 'ExampleClass' => App\Example\ExampleClass::class,
+        'Clockwork' => Clockwork\Support\Laravel\Facade::class,
     ])->toArray(),
 
 ];
