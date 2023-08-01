a9226b80f4e055437b923d793b8db70b0de94477
manirabona-programer <hseal419@gmail.com>
2023-08-01 12:07:29 +0200

    chore: products card responsivness

--- resources/js/Components/pages/exports.vue
+++ resources/js/Components/pages/exports.vue
@@ -30,7 +30,7 @@ onMounted(async () => await getExports());
     <div>
         <div class="p-6 lg:p-8 bg-white dark:bg-gray-800 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent">
             <div class='mb-5'>
-                <div class="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-gray-200 dark:bg-gray-700 overflow-hidden">
+                <div class="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-gray-100 dark:bg-gray-700 overflow-hidden">
                     <div class="grid place-items-center h-full w-12 text-gray-300">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
@@ -38,7 +38,7 @@ onMounted(async () => await getExports());
                     </div>
 
                     <input
-                        class="peer h-full w-full outline-none border-none text-sm text-gray-700 pr-2 dark:bg-gray-700 dark:text-gray-100"
+                        class="peer h-full w-full outline-none border-none text-sm bg-gray-100 text-gray-700 pr-2 dark:bg-gray-700 dark:text-gray-100"
                         type="text"
                         id="search"
                         @keypress.enter="searchExports($event.target.value)"
--- resources/js/Components/pages/products.vue
+++ resources/js/Components/pages/products.vue
@@ -174,7 +174,7 @@ onMounted(async () => {
         </div>
 
         <div class='my-10 mx-8'>
-            <div class="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-gray-200 dark:bg-gray-700 overflow-hidden">
+            <div class="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-gray-100 dark:bg-gray-700 overflow-hidden">
                 <div class="grid place-items-center h-full w-12 text-gray-300">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
@@ -182,7 +182,7 @@ onMounted(async () => {
                 </div>
 
                 <input
-                    class="peer h-full w-full outline-none border-none text-sm text-gray-700 pr-2 dark:bg-gray-700 dark:text-gray-100"
+                    class="peer h-full w-full outline-none border-none text-sm bg-gray-100 text-gray-700 pr-2 dark:bg-gray-700 dark:text-gray-100"
                     type="text"
                     id="search"
                     @keypress.enter="searchProducts($event.target.value)"
